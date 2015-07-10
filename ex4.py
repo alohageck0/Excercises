851e27850773d9de1b56092d05bf2f4bd255b20a
alohageck0 <royalfiish@gmail.com>
2015-07-09 18:20:42 -0700

    reformatted

--- ex4.py
+++ ex4.py
@@ -3,3 +3,13 @@ x = raw_input('Enter sequence of numbers: ')
 y = x.split(",")
 print y
 print tuple(y)
+
+'''
+Question:
+Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple which contains every number.
+Suppose the following input is supplied to the program:
+34,67,55,33,12,98
+Then, the output should be:
+['34', '67', '55', '33', '12', '98']
+('34', '67', '55', '33', '12', '98')
+'''
--- ex5.py
+++ ex5.py
@@ -15,3 +15,11 @@ class Func():
 y = Func()
 y.getstring()
 y.toupper()
+
+'''
+Question:
+Define a class which has at least two methods:
+getString: to get a string from console input
+printString: to print the string in upper case.
+Also please include simple test function to test the class methods.
+'''
