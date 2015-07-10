a311115e2be783aafc7d512442a90cc915aaaf1c
alohageck0 <royalfiish@gmail.com>
2015-07-09 18:11:17 -0700

    solutions

--- ex2.py
+++ ex2.py
@@ -1,7 +1,8 @@
-fn = int(raw_input('Enter number: '))
-i = 1
-fact = fn
-while i <= (fn - 1):
-    fact = fact * (fn - i)
-    i += 1
-print fact
+def fact(x):
+    if x == 0:
+        return 1
+    return x * fact(x - 1)
+
+
+x = int(raw_input())
+print fact(x)
