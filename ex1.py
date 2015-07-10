9d4452de55efb953176bb9bc6cddfb832c2a6e9c
alohageck0 <royalfiish@gmail.com>
2015-07-09 18:10:42 -0700

    solutions

--- ex1.py
+++ ex1.py
@@ -1,5 +1,6 @@
-arr = []
-for i in range(2000, 3200, 1):
+l = []
+for i in range(2000, 3201):
     if (i % 7 == 0) and (i % 5 != 0):
-        arr.append(str(i))
-print ", ".join(arr)
+        l.append(str(i))
+
+print ','.join(l)
