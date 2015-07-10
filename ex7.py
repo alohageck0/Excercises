87784acfcd4870c96d53b383e3c3c6c324431d1b
alohageck0 <royalfiish@gmail.com>
2015-07-09 18:43:30 -0700

    reformatted

--- ex7.py
+++ ex7.py
@@ -5,8 +5,9 @@ test1 = "Github2"
 
 '''
 Question:
-Write a program which takes 2 digits, X,Y as input and generates a 2-dimensional array. The element value in the i-th row and j-th column of the array should be i*j.
-Note: i=0,1.., X-1; j=0,1,¡­Y-1.
+Write a program which takes 2 digits, X,Y as input and generates a 2-dimensional array.
+The element value in the i-th row and j-th column of the array should be i*j.
+Note: i=0,1.., X-1; j=0,1, Y-1.
 Example
 Suppose the following inputs are given to the program:
 3,5
