28237c283f29753c0feedc773d0ce2ef028a6d4e
alohageck0 <royalfiish@gmail.com>
2015-07-11 15:49:06 -0700

    update

--- ex6.py
+++ ex6.py
@@ -2,10 +2,10 @@ import math
 
 __author__ = 'royalfiish'
 result = []
-items = [x for x in (raw_input('Enter sequence of numbers: ').split(','))]
+items = [x for x in (input('Enter sequence of numbers: ').split(','))]
 for d in items:
     result.append(str(int(math.sqrt((2 * 50 * float(d)) / 30))))
-print (",").join(result)
+print((",").join(result))
 
 '''
 Question:
