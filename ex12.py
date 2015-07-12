65b1e8978ed9418f01059ea8653c600d4f7477d0
alohageck0 <royalfiish@gmail.com>
2015-07-12 14:51:54 -0700

    update

--- ex12.py
+++ ex12.py
@@ -1,14 +1,26 @@
 __author__ = 'royalfiish'
 
+
+def check_every_digit(num):
+    marker = int
+    for n in str(num):
+        if int(n) % 2:
+            marker = 1
+            break
+        else:
+            marker = 2
+    if not marker % 2:
+        return True
+    else:
+        return False
+
+
 arr = []
 for i in range(1000, 3001):
-    for j in str(i):
-        if not int(j) % 2:
-            continue
-        else:
-            arr.append(str(i))
-    continue
+    if check_every_digit(i):
+        arr.append(str(i))
 print(', '.join(arr))
+
 '''
 Write a program, which will find all such numbers between 1000 and 3000 (both included)
 such that each digit of the number is an even number.
