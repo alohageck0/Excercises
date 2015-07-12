6e0d6831a0bbfb747ca1adf8a07347609372beab
alohageck0 <royalfiish@gmail.com>
2015-07-11 22:17:13 -0700

    update

--- ex9.py
+++ ex9.py
@@ -1,5 +1,14 @@
 __author__ = 'royalfiish'
 
+lines = []
+while True:
+    line = input('Enter sentence: ')
+    if line:
+        lines.append(line.upper())
+    else:
+        break
+print('\n'.join(lines))
+
 '''Write a program that accepts sequence of lines as input and
 prints the lines after making all characters in the sentence capitalized.
 Suppose the following input is supplied to the program:
