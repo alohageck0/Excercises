e0a3a397d2d8d37c7db32a860ab0732a565b93b8
alohageck0 <royalfiish@gmail.com>
2015-07-11 21:53:49 -0700

    update

--- ex9.py
+++ ex9.py
@@ -1,1 +1,11 @@
 __author__ = 'royalfiish'
+
+'''Write a program that accepts sequence of lines as input and
+prints the lines after making all characters in the sentence capitalized.
+Suppose the following input is supplied to the program:
+Hello world
+Practice makes perfect
+Then, the output should be:
+HELLO WORLD
+PRACTICE MAKES PERFECT
+'''
