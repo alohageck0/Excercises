db6536beab60ebcd2430b2e6aba74701b8c81a46
alohageck0 <royalfiish@gmail.com>
2015-07-12 19:58:30 -0700

    init

--- ex13.py
+++ ex13.py
@@ -1,5 +1,26 @@
 __author__ = 'royalfiish'
 
+line = input('Enter sentence: ')
+clean_list = []
+symbols = '!@#$%^&*()_ +{}|:\";\'<>?,./'
+for letter in line:
+    for i in range(0, len(symbols)):
+        line = line.replace(symbols[i], "")
+        if len(letter) > 0:
+            clean_list.append(letter)
+
+# print(line)
+letters = 0
+digits = 0
+for ch in line:
+    try:
+        int(ch)
+        digits += 1
+    except:
+        letters += 1
+
+print('LETTERS ', letters, '\n', 'DIGITS ', digits)
+
 '''
 Write a program that accepts a sentence and calculate the number of letters and digits.
 Suppose the following input is supplied to the program:
