f3e77febd1012996a2f75b176b18fa2be9cd6825
alohageck0 <royalfiish@gmail.com>
2015-07-18 18:36:51 -0700

    update

--- ex23.py
+++ ex23.py
@@ -1,5 +1,19 @@
 __author__ = 'royalfiish'
 
+
+class Calc:
+    def __init__(self):
+        self.val = int
+
+    def square(self, value):
+        self.val = value ** 2
+        return self.val
+
+
+num = int(input("Enter number to calculate square value: "))
+number = Calc()
+print('Square number of', num, 'is', number.square(num))
+
 '''
 Question:
     Write a method which can calculate square value of number
