49fb2bd756f4039abae3d69eae2090e59812981d
alohageck0 <royalfiish@gmail.com>
2015-07-18 20:48:55 -0700

    update

--- ex2_11.py
+++ ex2_11.py
@@ -1,5 +1,16 @@
 __author__ = 'royalfiish'
 
+arr = []
+for strin in range(2):
+    arr.append(input("Enter string: "))
+if len(arr[0]) > len(arr[1]):
+    print(arr[0])
+elif len(arr[0]) == len(arr[1]):
+    for strin in arr:
+        print(strin)
+else:
+    print(arr[1])
+
 '''
 Question:
 Define a function that can accept two strings as input and print the string with maximum length in console.
