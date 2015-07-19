d50b2bda473f0b790c51fe810c6357ee91dbb432
alohageck0 <royalfiish@gmail.com>
2015-07-18 21:24:20 -0700

    update

--- ex2_14.py
+++ ex2_14.py
@@ -1,5 +1,15 @@
 __author__ = 'royalfiish'
 
+
+def get_dict(n):
+    numbers = dict()
+    for num in range(1, n + 1):
+        numbers[num] = num ** 2
+    for k, v in numbers.items():
+        print(k, '-', v)
+
+
+get_dict(20)
 '''
 Question:
     Define a function which can print a dictionary where the keys are numbers between 1 and 20 (both included)
