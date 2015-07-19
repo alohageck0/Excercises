2a9dfa5d7c05c9feb1b1c78af48a3dcc148f90b7
alohageck0 <royalfiish@gmail.com>
2015-07-19 15:53:37 -0700

    update

--- ex2_17.py
+++ ex2_17.py
@@ -1,5 +1,11 @@
 __author__ = 'royalfiish'
 
+strin = input("Enter Yes or any: ")
+if strin.lower() == 'yes':
+    print('Yes')
+else:
+    print("No")
+
 '''
 Question:
     Write a program which accepts a string as input to print "Yes" if the string is "yes"
