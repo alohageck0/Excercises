33d466133020e8595c77fcfd9d6b6385c49bc42f
alohageck0 <royalfiish@gmail.com>
2015-07-19 15:43:59 -0700

    update

--- ex2_16.py
+++ ex2_16.py
@@ -1,12 +1,17 @@
+
 __author__ = 'royalfiish'
 
-'''
-Question:
-    Write a program to generate and print another tuple whose values
-    are even numbers in the given tuple (1,2,3,4,5,6,7,8,9,10).
+tup = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+tup_even = tuple
+for num in tup:
+    if not num
+    
 
-Hints:
-
-    Use "for" to iterate the tuple
-    Use tuple() to generate a tuple from a list.
-'''
+# Question:
+#     Write a program to generate and print another tuple whose values
+#     are even numbers in the given tuple (1,2,3,4,5,6,7,8,9,10).
+# 
+# Hints:
+# 
+#     Use "for" to iterate the tuple
+#     Use tuple() to generate a tuple from a list.
