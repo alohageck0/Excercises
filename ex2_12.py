41155ec640cf12b26dd79b0ef50ccba90bcd1135
alohageck0 <royalfiish@gmail.com>
2015-07-18 20:56:46 -0700

    update

--- ex2_12.py
+++ ex2_12.py
@@ -1,5 +1,17 @@
 __author__ = 'royalfiish'
 
+num = int(input("Enter number: "))
+
+
+def check_even(number):
+    if not number % 2:
+        print("It is an even number")
+    else:
+        print("It is an odd number")
+
+
+check_even(num)
+
 '''
 Question:
     Define a function that can accept an integer number as input and print the
