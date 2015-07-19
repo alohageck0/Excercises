835bc4a8b70e7c90655decb668ffe97f25b319e0
alohageck0 <royalfiish@gmail.com>
2015-07-18 20:20:01 -0700

    init

--- ex2_17.py
+++ ex2_17.py
@@ -1,1 +1,11 @@
 __author__ = 'royalfiish'
+
+'''
+Question:
+    Write a program which accepts a string as input to print "Yes" if the string is "yes"
+    or "YES" or "Yes", otherwise print "No".
+
+Hints:
+
+    Use if statement to judge condition.
+'''
