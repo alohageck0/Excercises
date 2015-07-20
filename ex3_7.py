5aafa5d82830b4b4dfad4257e3e17c1c808f5a12
alohageck0 <royalfiish@gmail.com>
2015-07-19 17:33:48 -0700

    init

---
+++ ex3_7.py
@@ -0,0 +1,12 @@
+__author__ = 'royalfiish'
+
+'''
+Question:
+    Write a program which can filter() to make a list whose
+    elements are even number between 1 and 20 (both included).
+
+Hints:
+
+    Use filter() to filter elements of a list.
+    Use lambda to define anonymous functions.
+'''
