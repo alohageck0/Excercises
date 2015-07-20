50a587147e2df8bc34e278707107785a8aac1343
alohageck0 <royalfiish@gmail.com>
2015-07-19 17:43:59 -0700

    init

---
+++ ex7_2.py
@@ -0,0 +1,10 @@
+__author__ = 'royalfiish'
+
+'''
+Question:
+    Define a class named American which has a static method called printNationality.
+
+Hints:
+
+    Use @staticmethod decorator to define class static method.
+'''
