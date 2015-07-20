cd9fe42b4af001f8af7ee7cbc66d119203170e85
alohageck0 <royalfiish@gmail.com>
2015-07-20 14:53:40 -0700

    init

---
+++ ex29.py
@@ -0,0 +1,9 @@
+__author__ = 'royalfiish'
+
+'''
+    Define a custom exception class which takes a string message as attribute.
+
+Hints:
+
+    To define a custom exception, we need to define a class inherited from Exception.
+'''
