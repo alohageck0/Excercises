189f0baacf50792e83c16a072b25591da57432a5
alohageck0 <royalfiish@gmail.com>
2015-07-20 16:59:38 -0700

    init

--- ex29.py
+++ ex29.py
@@ -1,5 +1,13 @@
 __author__ = 'royalfiish'
 
+
+class PrintMyError(Exception):
+    def __init__(self, str):
+        self.strin = str
+
+
+raise PrintMyError("Exception occurred")
+
 '''
     Define a custom exception class which takes a string message as attribute.
 
