736b19b18e8506afa687ae5d813ce2c288c31fa7
alohageck0 <royalfiish@gmail.com>
2015-07-19 17:49:22 -0700

    update

--- ex7_2.py
+++ ex7_2.py
@@ -1,5 +1,14 @@
 __author__ = 'royalfiish'
 
+
+class American:
+    @staticmethod
+    def printNationality(val):
+        print(val)
+
+
+American.printNationality('American')
+
 '''
 Question:
     Define a class named American which has a static method called printNationality.
