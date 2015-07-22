a630742e4c6d04594d76984b9a7e88aac8af8ca3
alohageck0 <royalfiish@gmail.com>
2015-07-22 12:18:41 -0700

    update

--- ex35.py
+++ ex35.py
@@ -1,12 +1,24 @@
 __author__ = 'royalfiish'
 
+inp = int(input("Enter n: "))
+
+
+def comp(n):
+    if n > 0:
+        return comp(n - 1) + 100
+    elif n == 0:
+        return 0
+
+
+print(comp(inp))
+
 '''
 Question:
 
     Write a program to compute:
 
     f(n)=f(n-1)+100 when n>0
-    and f(0)=1
+    and f(0)=0
 
     with a given n input by console (n>0).
 
