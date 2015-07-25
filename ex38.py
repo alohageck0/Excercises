6e3f6f89511f733b2f0a3d992ac0bd2f7e943d81
alohageck0 <royalfiish@gmail.com>
2015-07-24 18:00:45 -0700

    init

--- ex38.py
+++ ex38.py
@@ -1,9 +1,16 @@
 __author__ = 'royalfiish'
 
+inp = input("Enter n: ")
+
+
+def foo():
+
+
 '''
 Question:
 
-Please write a program using generator to print the even numbers between 0 and n in comma separated form while n is input by console.
+Please write a program using generator to print the even numbers between 0 and n in comma separated form
+while n is input by console.
 
 Example:
 If the following n is given as input to the program:
