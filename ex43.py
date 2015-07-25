b4bfaf4d469c69c4c4e7fa8d5641c2d6fb83ccc6
alohageck0 <royalfiish@gmail.com>
2015-07-25 15:38:37 -0700

    init

---
+++ ex43.py
@@ -0,0 +1,11 @@
+__author__ = 'royalfiish'
+
+'''
+Please generate a random float where the value is between 5 and 95 using Python math module.
+
+
+
+Hints:
+Use random.random() to generate a random float in [0,1].
+
+'''
