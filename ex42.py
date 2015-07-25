fa2cfcd956d316bc838398cd408e0e2dc06d98d9
alohageck0 <royalfiish@gmail.com>
2015-07-24 19:52:31 -0700

    init

---
+++ ex42.py
@@ -0,0 +1,17 @@
+__author__ = 'royalfiish'
+
+lis = [1, 4, 6, 3, 5, 7, 8, 9, 4, 3, 4, 5, 2]
+lis1 = set(lis)
+# print(lis1)
+
+
+'''
+Question:
+
+Please write a binary search function which searches an item in a sorted list.
+The function should return the index of element to be searched in the list.
+
+
+Hints:
+Use if/elif to deal with conditions.
+'''
