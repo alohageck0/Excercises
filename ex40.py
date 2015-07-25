21ecd45713036e0ab91464cb251bb964bda7d41b
alohageck0 <royalfiish@gmail.com>
2015-07-24 18:34:11 -0700

    init

---
+++ ex40.py
@@ -0,0 +1,13 @@
+__author__ = 'royalfiish'
+
+'''
+Question:
+
+
+Please write assert statements to verify that every number in the list [2,4,6,8] is even.
+
+
+
+Hints:
+Use "assert expression" to make assertion.
+'''
