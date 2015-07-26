e0715d11add222bdfdabf2132d79a898b0bd57ec
alohageck0 <royalfiish@gmail.com>
2015-07-26 14:52:22 -0700

    init

---
+++ ex44.py
@@ -0,0 +1,12 @@
+__author__ = 'royalfiish'
+
+'''
+Question:
+
+Please write a program to output a random even number between 0 and 10 inclusive using random module and list comprehension.
+
+
+
+Hints:
+Use random.choice() to a random element from a list.
+'''
