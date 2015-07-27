05e39906fda297a884aa0a338ba8d16f181a645f
alohageck0 <royalfiish@gmail.com>
2015-07-27 11:54:44 -0700

    init

--- ex43.py
+++ ex43.py
@@ -1,4 +1,8 @@
 __author__ = 'royalfiish'
+import random
+
+for i in range(300):
+    print(int(random.random() * 91) + 5)
 
 '''
 Please generate a random float where the value is between 5 and 95 using Python math module.
--- ex44.py
+++ ex44.py
@@ -1,9 +1,14 @@
 __author__ = 'royalfiish'
+import random
+
+for x in range(200):
+    print(random.choice([x for x in range(0, 11)]))
 
 '''
 Question:
 
-Please write a program to output a random even number between 0 and 10 inclusive using random module and list comprehension.
+Please write a program to output a random even number between 0 and 10
+inclusive using random module and list comprehension.
 
 
 
