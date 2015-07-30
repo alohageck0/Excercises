44cdae2f6e1485664b1c3b21d06074cf0e00be97
alohageck0 <royalfiish@gmail.com>
2015-07-29 20:44:30 -0700

    init

---
+++ ex47.py
@@ -0,0 +1,12 @@
+__author__ = 'royalfiish'
+
+'''
+Question:
+
+Please write a program to randomly generate a list with 5 even numbers between 100 and 200 inclusive.
+
+
+
+Hints:
+Use random.sample() to generate a list of random values.
+'''
