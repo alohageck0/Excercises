7c03ef5262080280a40377ad7bc0364a6a570dda
alohageck0 <royalfiish@gmail.com>
2015-07-29 20:49:00 -0700

    init

---
+++ ex48.py
@@ -0,0 +1,13 @@
+__author__ = 'royalfiish'
+
+'''
+Question:
+
+Please write a program to randomly generate a list with 5 numbers, which are divisible by 5 and 7 ,
+between 1 and 1000 inclusive.
+
+
+
+Hints:
+Use random.sample() to generate a list of random values.
+'''
