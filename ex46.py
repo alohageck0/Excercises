660c9ce6c12c83a097b727cb21da62b2d48caa89
alohageck0 <royalfiish@gmail.com>
2015-07-29 17:16:30 -0700

    init

---
+++ ex46.py
@@ -0,0 +1,12 @@
+__author__ = 'royalfiish'
+
+'''
+Question:
+
+    Please write a program to generate a list with 5 random numbers between 100 and 200 inclusive.
+
+
+
+Hints:
+    Use random.sample() to generate a list of random values.
+'''
