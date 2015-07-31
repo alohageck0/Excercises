b568bf17d33a6dd1c81a2c4ee9c9e7bf5458f7f7
alohageck0 <royalfiish@gmail.com>
2015-07-31 10:21:25 -0700

    init

--- generator.py
+++ generator.py
@@ -1,1 +1,8 @@
 __author__ = 'royalfiish'
+
+x = range(1, 10)
+
+
+def comp(n):
+    for i in x:
+        if
--- google_Peculiar_balance.py
+++ google_Peculiar_balance.py
@@ -1,71 +1,33 @@
 __author__ = 'royalfiish'
 
 
-def gen_lis(n):
-    global mass
-    mass = [1]
-    while mass[-1] < n:
-        if mass[-1] * 3 < n:
-            mass.append(mass[-1] * 3)
-        else:
-            break
-    return mass
-
-
-def compare(weight):
-    global left_side
-    global right_side
-    left_side = weight
-    ostatok =
+def get_indecies(weight):
+    global left
+    global right
+    left = weight
+    right = 0
+    indecies_arr = [x for x in range(100)]
+    indecies_dict = dict()
     while not left == right:
+        if (left - right) == 1:
+            right += 1
+            indecies_dict[0] = 'R'
+            continue
+        elif (right - left) == 1:
+            left += 1
+            indecies_dict[0] = 'L'
+            continue
+        elif (left - right) == 3:
+            right += 3
+            indecies_dict[1] = 'R'
+            continue
+        elif (right - left) == 3:
+            left += 3
+            indecies_dict[1] = 'L'
+            continue
+
+    return indecies_dict
 
 
-# def compare(left_weight):
-
-
-'''
-Peculiar balance
-================
-
-Can we save them? Beta Rabbit is trying to break into a lab that contains the only known zombie cure -
-but there's an obstacle. The door will only open if a challenge is solved correctly.
-The future of the zombified rabbit population is at stake, so Beta reads the challenge:
-There is a scale with an object on the left-hand side, whose mass is given in some number of units.
-Predictably, the task is to balance the two sides. But there is a catch: You only have this peculiar weight set,
-having masses 1, 3, 9, 27, ... units. That is, one for each power of 3. Being a brilliant mathematician,
-Beta Rabbit quickly discovers that any number of units of mass can be balanced exactly using this set.
-
-To help Beta get into the room, write a method called answer(x),
-which outputs a list of strings representing where the weights should be placed,
-in order for the two sides to be balanced, assuming that weight on the left has mass x units.
-
-The first element of the output list should correspond to the 1-unit weight,
-the second element to the 3-unit weight, and so on. Each string is one of:
-
-"L" : put weight on left-hand side
-"R" : put weight on right-hand side
-"-" : do not use weight
-
-To ensure that the output is the smallest possible, the last element of the list must not be "-".
-
-x will always be a positive integer, no larger than 1000000000.
-
-Languages
-=========
-
-To provide a Python solution, edit solution.py
-To provide a Java solution, edit solution.java
-
-Test cases
-==========
-
-Inputs:
-    (int) x = 2
-Output:
-    (string list) ["L", "R"]
-
-Inputs:
-    (int) x = 8
-Output:
-(string list) ["L", "-", "R"]
-'''
+x = get_indecies(3)
+print(x)
