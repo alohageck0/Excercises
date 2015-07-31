5311f218dbf80485281a42a32172b5abf9f39eab
alohageck0 <royalfiish@gmail.com>
2015-07-31 15:55:35 -0700

    init

--- generator.py
+++ generator.py
@@ -1,8 +1,13 @@
 __author__ = 'royalfiish'
 
-x = range(1, 10)
-
-
-def comp(n):
-    for i in x:
-        if
+right = 0
+summ = 0
+right += 1
+summ += right
+print(right, summ)
+right += 1
+summ += right
+print(right, summ)
+right += 1
+summ += right
+print(right, summ)
--- google_Peculiar_balance.py
+++ google_Peculiar_balance.py
@@ -7,6 +7,8 @@ def get_indecies(weight):
     global left_arr
     global right_arr
     global summ
+    global index_dict
+    index_dict = dict()
     left = weight
     right = 0
     index_arr = [i for i in range(100)]
@@ -15,33 +17,33 @@ def get_indecies(weight):
     while True:
         if left > right:
             for item in index_arr:
+                summ = right
                 right += 3 ** item
-                summ += right
                 right_arr.append(item)
                 print('R')
-                if (left + summ) > 3 ** item:
+                if (left + summ) > right:
                     right = 0
                     right_arr = []
                     continue
-                elif (left + summ) == 3 ** item:
+                elif (left + summ) == right:
                     left_arr = right_arr
                     right_arr = [item]
-
                 else:
                     break
 
-        elif left < right:
-            for item in index_arr:
-                left += 3 ** item
-                left_arr[item] = 'L'
-                print('L')
-                if (right - left) >= 3 ** index_arr[item + 1]:
-                    continue
-                else:
-                    break
+        # elif left < right:
+        #     for item in index_arr:
+        #         left += 3 ** item
+        #         left_arr[item] = 'L'
+        #         print('L')
+        #         if (right - left) >= 3 ** index_arr[item + 1]:
+        #             continue
+        #         else:
+        #             break
         elif left == right:
             break
-    return left_arr
+
+    print(right_arr)
 
 
 x = get_indecies(3)
