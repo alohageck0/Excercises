100a2f3442cd6bb8d96b7f1976362053fd5574d5
alohageck0 <royalfiish@gmail.com>
2015-07-31 13:08:52 -0700

    init

--- google_Peculiar_balance.py
+++ google_Peculiar_balance.py
@@ -4,51 +4,38 @@ __author__ = 'royalfiish'
 def get_indecies(weight):
     global left
     global right
+    global index_dict
     left = weight
     right = 0
     index_arr = [i for i in range(100)]
     index_dict = dict()
     while True:
-        if left == right:
-            return index_dict
-            break
-        elif left > right:
-            if (left - right) == 1:
-                right += 1
-                index_dict[0] = 'R'
-                continue
-            elif (left - right) == 3:
-                right += 3
-                index_dict[1] = 'R'
-                continue
+        if left > right:
             for item in index_arr:
                 right += 3 ** item
                 index_dict[item] = 'R'
-                if
+                print('R')
+                if left > right:
+                    right = 0
+                    index_dict[item] = '-'
+                    continue
+                elif
+                else:
+                    break
 
-
-
-
-
-
-
-
-
-        elif (right - left) == 1:
-            left += 1
-            index_dict[0] = 'L'
-            continue
-        elif (left - right) == 3:
-            right += 3
-            index_dict[1] = 'R'
-            continue
-        elif (right - left) == 3:
-            left += 3
-            index_dict[1] = 'L'
-            continue
-
+        elif left < right:
+            for item in index_arr:
+                left += 3 ** item
+                index_dict[item] = 'L'
+                print('L')
+                if left < right:
+                    continue
+                else:
+                    break
+        elif left == right:
+            break
     return index_dict
 
 
-x = get_indecies(3)
+x = get_indecies(4)
 print(x)
