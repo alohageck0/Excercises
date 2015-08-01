1124bea10c298b0e06f7afd372254acf1bead3fa
alohageck0 <royalfiish@gmail.com>
2015-08-01 14:33:21 -0700

    init

--- ex53.py
+++ ex53.py
@@ -1,5 +1,14 @@
 __author__ = 'royalfiish'
 
+arr1 = ["I", "You"]
+arr2 = ["Play", "Love"]
+arr3 = ["Hockey", "Football"]
+
+for i in range(len(arr1)):
+    for j in range(len(arr2)):
+        for k in range(len(arr3)):
+            print(arr1[i] + ' ' + arr2[j] + ' ' + arr3[k])
+
 '''
 Please write a program to generate all sentences where subject is in
 ["I", "You"] and verb is in ["Play", "Love"] and the object is in ["Hockey","Football"].
