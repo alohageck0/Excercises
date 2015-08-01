3dc502035959135d582f0ec71e9b1bce853adb6c
alohageck0 <royalfiish@gmail.com>
2015-08-01 15:27:43 -0700

    init

--- ex54.py
+++ ex54.py
@@ -1,5 +1,8 @@
 __author__ = 'royalfiish'
 
+lis = [5, 6, 77, 45, 22, 12, 24]
+print([x for x in lis if x % 2])
+
 '''
 Please write a program to print the list after removing delete even numbers in [5,6,77,45,22,12,24].
 
