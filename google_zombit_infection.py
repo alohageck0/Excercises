07c7e371bcbde8c40735f7c7d454fe9c38bfcf08
alohageck0 <royalfiish@gmail.com>
2015-08-01 21:17:16 -0700

    init

--- google_zombit_infection.py
+++ google_zombit_infection.py
@@ -1,9 +1,18 @@
 __author__ = 'royalfiish'
 
-popilation = [[1, 2, 3], [2, 3, 4], [3, 2, 1]]
+popilation = [[1, 2, 3],
+              [2, 3, 4],
+              [3, 2, 1]]
 
 
-def answer(population, x, y, strength):
+def answer(population, x, y, strength=None):
+    popilation[x][y] = -1
+    for x_col in popilation:
+        for y_row in x_col:
+            if popilation[x_col][y_row] == -1:
+
+
+
 
 
 '''
