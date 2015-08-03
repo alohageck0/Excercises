600850148208e7d171c8c14c4b3213cef189d256
alohageck0 <royalfiish@gmail.com>
2015-08-02 19:18:50 -0700

    update-7-12

--- google_zombit_infection.py
+++ google_zombit_infection.py
@@ -1,26 +1,26 @@
 __author__ = 'royalfiish'
 
-population = [[6, 7, 2, 7, 6], [6, 3, 1, 4, 7], [0, 2, 4, 1, 10], [8, 1, 1, 4, 9], [8, 7, 4, 9, 9]]
+popula = [[6, 7, 2, 7, 6], [6, 3, 1, 4, 7], [0, 2, 4, 1, 10], [8, 1, 1, 4, 9], [8, 7, 4, 9, 9]]
 population1 = [[1, 2, 3], [2, 3, 4], [3, 2, 1]]
 
 
-def answer(popul, y, x, strength=None):
-    global population
-    population = popul
-    x_max_row = len(population[0])
-    y_max_col = len(population)
+def answer(population, y, x, strength):
+    global popula
+    popula = population
+    x_max_row = len(popula[0])
+    y_max_col = len(popula)
 
     def next_round(popul, iterx):
-        global population
-        population = popul
+        global popula
+        popula = popul
         for i in range(iterx):
-            for x_row in population:
+            for x_row in popula:
                 for y_col in range(len(x_row)):
                     if x_row[y_col] == -1:
-                        neighbors = find_neighbors(population.index(x_row), y_col)
+                        neighbors = find_neighbors(popula.index(x_row), y_col)
                         for neighbor in neighbors:
-                            if population[neighbor[0]][neighbor[1]] <= strength:
-                                population[neighbor[0]][neighbor[1]] = -1
+                            if popula[neighbor[0]][neighbor[1]] <= strength:
+                                popula[neighbor[0]][neighbor[1]] = -1
 
     def find_neighbors(x, y):
         coords = [[] for i in range(4)]
@@ -36,20 +36,20 @@ def answer(popul, y, x, strength=None):
         clean_arr(coords)
         return coords
 
-    if population[x][y] <= strength:
-        population[x][y] = -1
+    if popula[x][y] <= strength:
+        popula[x][y] = -1
     else:
-        return population
+        return popula
     if x_max_row >= y_max_col:
         maxiter = x_max_row
     else:
         maxiter = y_max_col
-    next_round(population, maxiter)
+    next_round(popula, maxiter)
 
-    return population
+    return popula
 
 
-first = answer(population, 2, 1, 5)
+first = answer(popula, 2, 1, 5)
 print(first)
 print([[6, 7, -1, 7, 6], [6, -1, -1, -1, 7], [-1, -1, -1, -1, 10], [8, -1, -1, -1, 9], [8, 7, -1, 9, 9]])
 
