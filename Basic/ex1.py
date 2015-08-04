e692977a718da37675e3d160144d6576d548208a
alohageck0 <royalfiish@gmail.com>
2015-08-03 17:52:39 -0700

    init

--- Basic/ex1.py
+++ Basic/ex1.py
@@ -1,4 +1,5 @@
 __author__ = 'royalfiish'
 
-print(
-    "Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are")
+print("Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\t"
+      "Up above the world so high, \n\t\tLike a diamond in the sky. \n"
+      "Twinkle, twinkle, little star, \n\tHow I wonder what you are")
