bb66645070931e594a71cbf8d4e7efc227528af3
alohageck0 <royalfiish@gmail.com>
2015-08-07 19:27:36 -0700

    update

--- Hackerrank/DiagonalDifference.py
+++ Hackerrank/DiagonalDifference.py
@@ -1,7 +1,27 @@
 __author__ = 'royalfiish'
 
-arr = [[11, 2, 4],
-       [4, 5, 6],
-       [10, 8, -12]]
+array = [[11, 2, 4],
+         [4, 5, 6],
+         [10, 8, -12]]
 
-coords = [[] for x in range(2 * len(arr))]
+array1 = []
+inp = int(input())
+for i in range(inp):
+    inp = input()
+    digits = inp.split(' ')
+    array1.append([int(x) for x in digits])
+
+
+def foo(arr):
+    d1 = 0
+    d2 = 0
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            if i + j == (len(arr) - 1):
+                d1 += arr[i][j]
+            if i == j:
+                d2 += arr[i][j]
+    print(abs(d1 - d2))
+
+
+foo(array1)
