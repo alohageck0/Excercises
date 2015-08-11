002ded0362c09d7ecd21e94e570cf3cd5b59b79d
alohageck0 <royalfiish@gmail.com>
2015-08-11 14:08:09 -0700

    08/11/15

--- Hackerrank/Implementation/SherlockandTheBeast.py
+++ Hackerrank/Implementation/SherlockandTheBeast.py
@@ -1,10 +1,22 @@
 __author__ = 'royalfiish'
 
 loops = int(input())
-# for i in range(loops):
-# number = int(input())
-# lis = []
-# for j in range(10 ** (number - 1), 10 ** (number)):
-#     jStr = str(j)
-#     for k in range(len(jStr)):
-#         if jStr[k] in [3,5]:
+for i in range(loops):
+    number = int(input())
+lis = []
+
+
+def checkNumber(num):
+    check = True
+    numStr = str(num)
+    for k in range(len(numStr)):
+        if not numStr[k] in ['3', '5']:
+            check = False
+    return check
+
+
+for j in range(10 ** (number - 1), 10 ** (number)):
+    if checkNumber(j):
+        lis.append(j)
+
+        # print(lis)
