1a39788b114fc304524ea19ed63db981eb4c3ff8
alohageck0 <royalfiish@gmail.com>
2015-08-10 19:17:49 -0700

    08/10/15

--- Hackerrank/Extralongfactorials.py
+++ Hackerrank/Extralongfactorials.py
@@ -1,1 +1,16 @@
 __author__ = 'royalfiish'
+
+number = int(input())
+fact = number
+
+
+def factorial(num):
+    if num > 1:
+        global fact
+        fact *= num - 1
+        return factorial(num - 1)
+    else:
+        return fact
+
+
+print(factorial(number))
