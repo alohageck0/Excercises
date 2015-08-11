3cf7d5edbe33ccaddaebdbe66fb48fa40871f9d2
alohageck0 <royalfiish@gmail.com>
2015-08-10 22:32:30 -0700

    08/10/15

--- Hackerrank/Implementation/SherlockandTheBeast.py
+++ Hackerrank/Implementation/SherlockandTheBeast.py
@@ -1,1 +1,10 @@
 __author__ = 'royalfiish'
+
+loops = int(input())
+# for i in range(loops):
+# number = int(input())
+# lis = []
+# for j in range(10 ** (number - 1), 10 ** (number)):
+#     jStr = str(j)
+#     for k in range(len(jStr)):
+#         if jStr[k] in [3,5]:
