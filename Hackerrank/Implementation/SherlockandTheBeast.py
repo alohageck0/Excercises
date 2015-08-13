e60f725c5393c4c932d402f00d3067a48aa7a686
alohageck0 <royalfiish@gmail.com>
2015-08-12 17:01:45 -0700

    08/12/15

--- Hackerrank/Implementation/SherlockandTheBeast.py
+++ Hackerrank/Implementation/SherlockandTheBeast.py
@@ -4,6 +4,7 @@ loops = int(input())
 for i in range(loops):
     number = int(input())
 lis = []
+newlis = []
 
 
 def checkNumber(num):
@@ -17,28 +18,32 @@ def checkNumber(num):
 
 def check35(num):
     numStr = str(num)
-    for k in range(len(numStr)):
-        act3 = 0
-        act5 = 0
-        if k == 3:
+    act3 = 0
+    act5 = 0
+    for k in numStr:
+        if k == '3':
             act3 += 1
-            print(act3)
-        elif k == 5:
+        elif k == '5':
             act5 += 1
-            print(act5)
-        if not act3 % 5 and not act5 % 3:
+    if act3 >= 5 or act5 >= 3 or (act3 >= 5 and act5 >= 3):
+        if (not act3 % 5 and act5 == 0) or (not act5 % 3 and act3 == 0) or not act3 % 5 or not act5 % 3:
+            # print("act3 = ", act3)
+            # print("act5 = ", act5)
             return True
         else:
             return False
+    else:
+        return False
+
 
 for j in range(10 ** (number - 1), 10 ** (number)):
     if checkNumber(j):
         lis.append(j)
 
-# for item in lis:
-#     if check35(item):
-#         pass
-#     else:
-#         lis.remove(item)
-print(lis)
-print(check35(lis[3]))
+for item in lis:
+    if check35(item):
+        newlis.append(item)
+if len(newlis) == 0:
+    print(-1)
+else:
+    print(max(newlis))
--- sandbox.py
+++ sandbox.py
@@ -1,6 +1,25 @@
-act3 = 3
-act5 = 3
-if not act3 % 5 and not act5 % 3:
-    print('\n divisible')
-else:
-    print('NO')
+act3 = 0
+act5 = 0
+
+
+def check35(num):
+    numStr = str(num)
+    act3 = 0
+    act5 = 0
+    for k in numStr:
+        if k == '3':
+            act3 += 1
+        elif k == '5':
+            act5 += 1
+    if act3 >= 5 or act5 >= 3 or (act3 >= 5 and act5 >= 3):
+        if (not act3 % 5 and act5 == 0) or (not act5 % 3 and act3 == 0) or not act3 % 5 or not act5 % 3:
+            print("act3 = ", act3)
+            print("act5 = ", act5)
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+print(check35(333))
