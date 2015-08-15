51b6f3cd666a775aa9b21b5993f3ec50e8864f45
alohageck0 <royalfiish@gmail.com>
2015-08-14 17:56:52 -0700

    08/14/15

--- Hackerrank/Implementation/SherlockandTheBeast.py
+++ Hackerrank/Implementation/SherlockandTheBeast.py
@@ -1,16 +1,20 @@
 __author__ = 'royalfiish'
 
 loops = int(input())
+
+
+def count(num):
+    fives = num // 3
+
+
 for i in range(loops):
     numberdigits = int(input())
-    fives = numberdigits // 3
-    threes = numberdigits - fives * 3
 
-    if not threes % 5:
-        print('555' * fives + '33333' * threes)
-        # print(threes)
-    elif not numberdigits % 5:
-        threes = numberdigits // 5
-        print('33333' * threes)
-    else:
-        print(-1)
+    # if not threes % 5:
+    #     print('555' * fives + '33333' * threes)
+    #     # print(threes)
+    # elif not numberdigits % 5:
+    #     threes = numberdigits // 5
+    #     print('33333' * threes)
+    # else:
+    #     print(-1)
