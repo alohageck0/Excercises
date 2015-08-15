51c1645acd84ff491fdff20303a8d27d9ff767bd
alohageck0 <royalfiish@gmail.com>
2015-08-14 19:38:13 -0700

    08/14/15-almost

--- Hackerrank/Implementation/SherlockandTheBeast.py
+++ Hackerrank/Implementation/SherlockandTheBeast.py
@@ -4,14 +4,30 @@ loops = int(input())
 
 
 def count(num):
+    if num < 5:
+        if num == 3:
+            return '555'
+        else:
+            return -1
     arr = dict()
     fives = num // 3
     threes = 0
     while True:
+        if (fives * 3 + threes * 5) > num:
+            fives -= 1
+            continue
         if (fives * 3 + threes * 5) == num:
             arr['fives'] = fives
             arr['threes'] = threes
             return arr
+            break
+        elif threes * 5 == num:
+            arr['fives'] = 0
+            arr['threes'] = threes
+            return arr
+            break
+        elif fives < 0:
+            return -1
             break
         else:
             fives -= 1
@@ -22,4 +38,7 @@ def count(num):
 for i in range(loops):
     numberdigits = int(input())
     dic = count(numberdigits)
-    print('555' * dic['fives'] + '33333' * dic['threes'])
+    if type(dic) == dict:
+        print('555' * dic['fives'] + '33333' * dic['threes'])
+    else:
+        print(dic)
