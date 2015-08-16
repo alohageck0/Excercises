0d7e6faafd190dd6714f5db4e2ed806e737d444e
alohageck0 <royalfiish@gmail.com>
2015-08-16 16:51:33 -0700

    08/16/15

--- Hackerrank/Implementation/SherlockandSquares.py
+++ Hackerrank/Implementation/SherlockandSquares.py
@@ -1,1 +1,15 @@
+import math
+
 __author__ = 'royalfiish'
+loops = int(input())
+
+for i in range(loops):
+    arr = [int(x) for x in input().split(' ')]
+    count = 0
+    for num in range(arr[0], arr[1] + 1):
+        square = math.sqrt(num)
+        if int(square) == square:
+            count += 1
+        else:
+            continue
+    print(count)
--- sandbox.py
+++ sandbox.py
@@ -1,21 +1,12 @@
-def count(num):
-    arr = dict()
-    fives = num // 3
-    threes = 0
-    while True:
-        if (fives * 3 + threes * 5) == num:
-            arr['fives'] = fives
-            arr['threes'] = threes
-            break
-        else:
-            fives -= 1
-            threes += 1
-            continue
-    return arr
+import math
 
-
-for i in range(1):
-    numberdigits = 11
-
-    dic = count(numberdigits)
-    print('555' * dic['fives'] + '33333' * dic['threes'])
+x = math.sqrt(49)
+if int(x) == x:
+    print('Int')
+else:
+    print("no")
+x = math.sqrt(50)
+if int(x) == x:
+    print('Int')
+else:
+    print("no")
