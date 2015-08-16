d04412b1791b9403982323a0185554acbc3488c8
alohageck0 <royalfiish@gmail.com>
2015-08-16 14:42:17 -0700

    08/16/15

--- Hackerrank/Implementation/FindDigits.py
+++ Hackerrank/Implementation/FindDigits.py
@@ -2,4 +2,14 @@ __author__ = 'royalfiish'
 loops = int(input())
 
 for i in range(loops):
-    number = int(input())
+    number = input()
+    count = 0
+    for letter in number:
+        divisor = int(letter)
+        if not divisor:
+            continue
+        elif not int(number) % divisor:
+            count += 1
+        else:
+            continue
+    print(count)
