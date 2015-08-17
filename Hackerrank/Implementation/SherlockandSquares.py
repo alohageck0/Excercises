2502f1db13b1894cb6418ff6bea40116f78161df
alohageck0 <royalfiish@gmail.com>
2015-08-16 17:35:00 -0700

    08/16/15

--- Hackerrank/Implementation/SherlockandSquares.py
+++ Hackerrank/Implementation/SherlockandSquares.py
@@ -1,15 +1,27 @@
 import math
 
-__author__ = 'royalfiish'
 loops = int(input())
 
-for i in range(loops):
-    arr = [int(x) for x in input().split(' ')]
-    count = 0
-    for num in range(arr[0], arr[1] + 1):
-        square = math.sqrt(num)
+
+def findFirstSquare(first, last):
+    while first < last + 1:
+        square = math.sqrt(first)
         if int(square) == square:
-            count += 1
-        else:
-            continue
+            return int(square)
+        first += 1
+    return 0
+
+
+for i in range(loops):
+    count = 0
+    arr = [int(x) for x in input().split(' ')]
+    if findFirstSquare(arr[0], arr[1]):
+        count = 1
+        first = findFirstSquare(arr[0], arr[1])
+    else:
+        print(count)
+        continue
+    while first ** 2 <= arr[1]:
+        first = first ** 2 + first * 2 + 1
+        count += 1
     print(count)
--- sandbox.py
+++ sandbox.py
@@ -1,12 +1,15 @@
 import math
 
-x = math.sqrt(49)
-if int(x) == x:
-    print('Int')
-else:
-    print("no")
-x = math.sqrt(50)
-if int(x) == x:
-    print('Int')
-else:
-    print("no")
+
+# loops = int(input())
+
+def findFirstSquare(first, last):
+    while first < last + 1:
+        square = math.sqrt(first)
+        if int(square) == square:
+            return int(square)
+        first += 1
+    return 0
+
+
+print(findFirstSquare(3, 9))
