93f75d19352e55bc06814758084b98894e1782b5
alohageck0 <royalfiish@gmail.com>
2015-08-18 16:52:50 -0700

    08/18/15

--- Hackerrank/Sorting/InsertionSort-Part2.py
+++ Hackerrank/Sorting/InsertionSort-Part2.py
@@ -2,18 +2,16 @@ arr = [1, 4, 3, 5, 6, 2]
 len = len(arr)
 # len = int(input())
 # arr = [int(x) for x in input().split(' ')]
-maxnum = 0
 while True:
-    for i in range(1, len - 1):
+    count = 0
+    for i in range(1, len):
         if arr[i - 1] > arr[i]:
             tempor = arr[i - 1]
-            arr[i] = arr[i - 1]
-            arr[i - 1] = tempor
-            if maxnum < tempor:
-                maxnum = tempor
+            arr[i - 1] = arr[i]
+            arr[i] = tempor
+            break
         else:
-            continue
-    if arr[-1] == maxnum:
+            count += 1
+    if count == len - 1:
         break
-# todo break wile loop
-print(' '.join([str(x) for x in arr]))
+    print(' '.join([str(x) for x in arr]))
