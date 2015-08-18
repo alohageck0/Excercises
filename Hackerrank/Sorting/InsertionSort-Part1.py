cf065d3758b32b38b2418388d9a377f963c2c527
alohageck0 <royalfiish@gmail.com>
2015-08-17 18:48:34 -0700

    08/17/15

--- Hackerrank/Sorting/InsertionSort-Part1.py
+++ Hackerrank/Sorting/InsertionSort-Part1.py
@@ -1,9 +1,10 @@
-# arr = [2, 4, 5, 6, 7, 8, 3]
+# arr = [2, 3, 4, 5, 6, 7, 8, 9, 10, 1]
+# len = len(arr)
 len = int(input())
 arr = [int(x) for x in input().split(' ')]
 num = arr[-1]
 for i in reversed(range(len)):
-    if arr[i - 1] < num:
+    if arr[i - 1] <= num or i == 0:
         arr[i] = num
         break
     else:
