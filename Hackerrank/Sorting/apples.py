d10219a427ca9629343bd676415d72c0daf456e8
alohageck0 <royalfiish@gmail.com>
2015-08-20 10:49:39 -0700

    08/20/15

---
+++ Hackerrank/Sorting/apples.py
@@ -0,0 +1,21 @@
+bags = int(input())
+arr = [int(x) for x in input().split(' ')]
+total = 0
+
+
+def apples(arr):
+    global total
+    if not sum(arr) % 3:
+        return sum(arr)
+    while (sum(arr) - total) % 3:
+        for bag in arr:
+            if not bag:
+                arr.remove(bag)
+                continue
+            bag -= 1
+            total += 1
+        if
+
+
+print(total)
+print(not total % 3)
