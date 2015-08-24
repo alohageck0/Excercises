a6d42553aff057c9206fcf75be32dffaaff879d8
alohageck0 <royalfiish@gmail.com>
2015-08-24 11:39:41 -0700

    08/24/15

--- Hackerrank/GraphTheory/EvenTree.py
+++ Hackerrank/GraphTheory/EvenTree.py
@@ -4,5 +4,4 @@ arr = [10, 9]
 # for i in range(arr[1]):
 #     edges.append(tuple([int(x) for x in input().split(' ')]))
 children = dict
-for edge in reversed(edges):
-    ss
+# for edge in reversed(edges):
