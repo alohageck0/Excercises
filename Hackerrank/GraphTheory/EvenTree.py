92aa5f1e1ff9917cfd8a7524d08b7fcab3ed66ed
alohageck0 <royalfiish@gmail.com>
2015-08-24 23:44:41 -0700

    08/24/15

--- Hackerrank/GraphTheory/EvenTree.py
+++ Hackerrank/GraphTheory/EvenTree.py
@@ -3,9 +3,14 @@ arr = [10, 9]
 # # arr = [int(x) for x in input().split(' ')]
 # for i in range(arr[1]):
 #     edges.append(tuple([int(x) for x in input().split(' ')]))
-# for edge in reversed(edges):
-matrix = [[0 for x in range(10)] for x in range(10)]
-for edge in edges:
-    matrix[edge[1] - 1][edge[0] - 1] = 1
-for i in matrix:
-    print(i)
+
+# matrix = [[0 for x in range(10)] for x in range(10)]
+# for edge in edges:
+#    matrix[edge[1] - 1][edge[0] - 1] = 1
+# for i in matrix:
+#    print(i)
+
+edges_d = dict
+# for edge in edges:
+#    edges_d[edge[1]] = edge[0]
+print(edges_d)
