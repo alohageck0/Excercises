a42106c65ecf1bf536bae22ca627e24e2332b227
alohageck0 <royalfiish@gmail.com>
2015-08-26 11:07:10 -0700

    08/26/15

--- Hackerrank/GraphTheory/EvenTree.py
+++ Hackerrank/GraphTheory/EvenTree.py
@@ -1,16 +1,22 @@
 edges = [(2, 1), (3, 1), (4, 3), (5, 2), (6, 1), (7, 2), (8, 6), (9, 8), (10, 8)]
 arr = [10, 9]
+
+
 # # arr = [int(x) for x in input().split(' ')]
 # for i in range(arr[1]):
 #     edges.append(tuple([int(x) for x in input().split(' ')]))
 
-# matrix = [[0 for x in range(10)] for x in range(10)]
-# for edge in edges:
-#    matrix[edge[1] - 1][edge[0] - 1] = 1
-# for i in matrix:
-#    print(i)
 
-edges_d = dict()
-# for edge in edges:
-#    edges_d[edge[1]] = edge[0]
-print(edges_d)
+def make_dic(edges):
+    edges_d = dict()
+    for edge in edges:
+        if not edge[1] in edges_d.keys():
+            edges_d[edge[1]] = [edge[0]]
+        else:
+            edges_d.get(edge[1]).append(edge[0])
+    return edges_d
+
+
+print(make_dic(edges))
+edges = [(3, 1), (4, 3), (5, 2), (6, 1), (7, 2), (8, 6), (9, 8), (10, 8)]
+print(sorted(make_dic(edges)))
