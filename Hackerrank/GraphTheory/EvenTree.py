3c82134889fb8dc03fdd20c7d66049abcd022db4
alohageck0 <royalfiish@gmail.com>
2015-09-06 23:21:16 -0700

    09/06/15 - test

--- Hackerrank/GraphTheory/EvenTree.py
+++ Hackerrank/GraphTheory/EvenTree.py
@@ -1,10 +1,11 @@
-edges = [(2, 1), (3, 1), (4, 3), (5, 2), (6, 1), (7, 2), (8, 6), (9, 8), (10, 8)]
-arr = [10, 9]
+# edges = [(2, 1), (3, 1), (4, 3), (5, 2), (6, 1), (7, 2), (8, 6), (9, 8), (10, 8)]
+# arr = [10, 9]
 
+edges = []
+arr = [int(x) for x in input().split(' ')]
+for i in range(arr[1]):
+    edges.append(tuple([int(x) for x in input().split(' ')]))
 
-# # arr = [int(x) for x in input().split(' ')]
-# for i in range(arr[1]):
-#     edges.append(tuple([int(x) for x in input().split(' ')]))
 
 # {8: [9, 10], 1: [2, 3, 6], 2: [5, 7], 3: [4], 6: [8]}
 class Graph:
@@ -20,77 +21,6 @@ class Graph:
                 self.edges_d[edge[1]] = [edge[0]]
             else:
                 self.edges_d.get(edge[1]).append(edge[0])
-
-    def add_to_list(self, list):
-        self.final.extend(list)
-        for i in self.final:
-            self.add_to_list(self.edges_d.get(i))
-
-    def children_of_node(self, parent):
-        count = 0
-        if parent in self.edges_d.keys():
-            count += self.count_nodes(parent)
-        else:
-            return count
-        temp = self.get_children_list(parent)
-
-        for i in temp:
-            count += self.count_nodes(i)
-            temp
-
-    def get_children_list(self, node):
-        if node in self.edges_d.keys():
-            return self.edges_d.get(node)
-
-    def count_nodes(self, node):
-        if node in self.edges_d.keys():
-            return len(self.edges_d.get(node))
-        else:
-            return 0
-
-    def check_key(self, node):
-        # self.check_key = []
-        # self.children = 0
-        # self.temp = []
-        if node in self.edges_d.keys():
-            return self.edges_d.get(node)
-        else:
-            return self.check_key
-
-    def foo(self, list):
-        if not len(list):
-            return
-        self.children += len(self.temp)
-        footemp = list
-        self.temp = []
-        for i in footemp:
-            if i in test.edges_d.keys():
-                self.temp.extend(test.edges_d.get(i))
-            else:
-                continue
-        return foo(self.temp)
-
-        # def get_list_of_nodes(graph, x):
-
-
-# test
-
-# def count_child(graph, node):
-#     count = 0
-#     if node in graph.edges_d.keys():
-#         count += len(graph.edges_d.get(node))
-#     temp = graph.edges_d.get(node)
-#     print(temp, count)
-#
-#     # for child in temp:
-#     #     count += graph.count_nodes(child)
-#     #     return count_child(graph, child)
-#     # return count
-#
-#     # if
-#     #     count = 0
-#     #     # for node in dict1.get(key):
-#     # return count
 
 
 test = Graph(edges, arr)
@@ -123,17 +53,12 @@ def foo(list):
     return foo(temp)
 
 
-# foo(check_key(4))
-# print(children)
-
 for node in range(2, arr[0] + 1):
     children = 0
     temp = check_key(node)
     foo(temp)
-    # print(children)
     if not children % 2 and children != 0:
         count += 1
     else:
         continue
-# print(children)
 print(count)
