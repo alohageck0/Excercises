9d3120c7251777c9fcde1923cc8cdeb43592228c
alohageck0 <royalfiish@gmail.com>
2015-10-04 20:06:41 -0700

    10/04/15

--- Hackerrank/Implementation/TheGridSearch.py
+++ Hackerrank/Implementation/TheGridSearch.py
@@ -1,2 +1,3 @@
-arr = [int(x) for x in input().split(" ")]
-matrix = [int(x) for x in input().split(" ")]
+cases = int(input())
+for i in range(cases):
+    RandC = [int(x) for x in input().split(" ")]
