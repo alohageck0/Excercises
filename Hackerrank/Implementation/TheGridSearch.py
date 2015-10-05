0df96ddc4e1783ef8ea6d2877295b79b411c281e
alohageck0 <royalfiish@gmail.com>
2015-10-04 21:54:42 -0700

    10/04/15

--- Hackerrank/Implementation/TheGridSearch.py
+++ Hackerrank/Implementation/TheGridSearch.py
@@ -4,7 +4,7 @@ cases = int(input())
 def addElem(r):
     matrix = []
     for i in range(r):
-        matrix.append([int(x) for x in input().split()])
+        matrix.append(input())
     return matrix
 
 
@@ -14,6 +14,16 @@ def getMatrix():
     return matrix
 
 
+# G = ['7283455864', '6731158619', '8988242643',
+#      '3830589324', '2229505813', '5633845374', '6473530293',
+#      '7053106601', '0834282956', '4607924137']
+#
+# P = ['9505', '3845', '3530']
 for i in range(cases):
     G = getMatrix()
     P = getMatrix()
+    print(P)
+    print(G)
+    # for elem in G:
+    #    if P[0] in elem:
+    #        print('Yes')
