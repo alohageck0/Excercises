339d4d3dfb3745dc0ff92aca6dd619ac3d2d911f
alohageck0 <royalfiish@gmail.com>
2015-10-13 20:24:19 -0700

    10/12/15 wrong

--- tets1.py
+++ tets1.py
@@ -2,21 +2,41 @@ G = ['7283455864',
      '6731158619',
      '8988242643',
      '3830589324',
-     '1239503950',
+     '139502395055395022',
      '5633845374',
      '6473530293']
 
 P = ['3950',
      '5374',
      '0293']
+nextIndex = 0
 
 arr = []
-for elem in G:
+
+
+def findIndex(elem):
+    global nextIndex
+    global arr
+    global P
     for i in range(len(elem)):
         if elem[i] == P[0][0]:
-            if elem[i] == P[0]:
+            if elem.index(P[0]) == i:
                 arr.append(i)
+                nextIndex += (i + len(P[0]))
+    if nextIndex >= len(elem):
+        print(arr)
+    else:
+        return findIndex(elem[nextIndex:])
 
-print(arr)
+
+# smallLen = len(P[0])
+# tempIndex = int
+# for elem in G:
+#     findIndex(elem)
+findIndex(G[4])
+
+
+
+
 # todo first find index of first digit - then split from that index by number of small number,
 # then look reminder of element for first digit and split - at the end find indecies of large number
