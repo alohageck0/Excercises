23938980b5dd2e7d54ee1d10b894727405146db1
alohageck0 <royalfiish@gmail.com>
2015-10-22 23:55:02 -0700

    10/22/15 almost

--- tets1.py
+++ tets1.py
@@ -2,11 +2,11 @@ G = ['7283455864',
      '6731158619',
      '8988242643',
      '3830589324',
-     '139502395055395022',
+     '131111111111112',
      '5633845374',
      '6473530293']
 
-P = ['3950',
+P = ['111',
      '5374',
      '0293']
 
@@ -18,10 +18,23 @@ def findNewIndex(elem):
     if P[0] in elem:
         ind = elem.index(P[0])
         if not arr:
-            arr.append(elem.index(P[0]))
+            arr.append(ind)
         else:
-            arr.append(arr[-1] + len(P[0]) + ind)
-    elem = elem[len(P[0]) + ind:]
+            arr.append(arr[-1] + ind)
+    elem = elem[ind:]
+    if P[0] not in elem:
+        print(arr)
+    else:
+        return findNewIndex(elem)
+
+
+def findNewIndexNew(elem):
+    global arr
+    if P[0] in elem:
+        for i in range(len(elem)):
+            ind = elem.index(P[0])
+            if i == ind:
+                arr.append(i)
     if P[0] not in elem:
         print(arr)
     else:
