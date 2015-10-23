55c9cf6912faf321036f7c20284d7e16160cc464
alohageck0 <royalfiish@gmail.com>
2015-10-23 00:08:25 -0700

    10/23/15 almost

--- tets1.py
+++ tets1.py
@@ -1,50 +1,30 @@
 G = ['7283455864',
      '6731158619',
-     '8988242643',
+     '8911111142643',
      '3830589324',
      '131111111111112',
      '5633845374',
      '6473530293']
 
-P = ['111',
+P = ['1111',
      '5374',
      '0293']
 
 arr = []
 
 
-def findNewIndex(elem):
-    global arr
-    if P[0] in elem:
-        ind = elem.index(P[0])
-        if not arr:
-            arr.append(ind)
-        else:
-            arr.append(arr[-1] + ind)
-    elem = elem[ind:]
-    if P[0] not in elem:
-        print(arr)
-    else:
-        return findNewIndex(elem)
-
-
-def findNewIndexNew(elem):
-    global arr
-    if P[0] in elem:
-        for i in range(len(elem)):
-
-            ind = elem.index(P[0])
-            if i == ind:
-                arr.append(i)
-    print(arr)
-
-
-def find_characters(word, character):
+def find_characters(strin, pattern):
+    """
+    Returns array of indexes of first row of pattern
+    :param strin:
+    :param pattern:
+    :return:
+    """
     found = []
     last_index = -1
     while True:
         try:
-            last_index = word.index(character, last_index + 1)
+            last_index = strin.index(pattern, last_index + 1)
         except ValueError:
             break
         else:
