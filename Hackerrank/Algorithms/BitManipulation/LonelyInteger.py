ac2161edd2200d0ec1a5d626d722d90274ee9778
alohageck0 <royalfiish@gmail.com>
2015-10-26 22:49:45 -0700

    10/26/15

--- Hackerrank/Algorithms/BitManipulation/LonelyInteger.py
+++ Hackerrank/Algorithms/BitManipulation/LonelyInteger.py
@@ -1,2 +1,11 @@
 numbers = int(input())
 arr = [int(x) for x in input().split(' ')]
+# arr = [1, 1, 1, 1, 2, 3, 3, 3, 2, 2, 4, 4, 2, 2, 9]
+result = dict()
+for elem in arr:
+    if elem not in result.keys():
+        result[elem] = 1
+    else:
+        result[elem] += 1
+reverse = {v: k for k, v in result.items()}
+print(reverse.get(1))
