06bc61146417054755e330fa8eb724501d9a57f0
alohageck0 <royalfiish@gmail.com>
2015-10-31 20:19:33 -0700

    10/31/15

--- Hackerrank/Algorithms/Strings/AlternatingCharacters.py
+++ Hackerrank/Algorithms/Strings/AlternatingCharacters.py
@@ -1,3 +1,11 @@
 loops = int(input())
 for i in range(loops):
-    number = int(input())
+    strin = input()
+    deletions = 0
+    letter = strin[0]
+    for i in range(1, len(strin)):
+        if strin[i] == letter:
+            deletions += 1
+        else:
+            letter = strin[i]
+    print(deletions)
--- sandbox.py
+++ sandbox.py
@@ -1,24 +1,9 @@
-def convert32bit(num):
-    binum = bin(num)[2:]
-    if len(binum) < 32:
-        binum32 = "0" * (32 - len(binum)) + binum
-        return binum32
+strin = "BBBBB"
+deletions = 0
+letter = strin[0]
+for i in range(1, len(strin)):
+    if strin[i] == letter:
+        deletions += 1
     else:
-        return binum
-
-
-def flip(strin):
-    flipped = str()
-    for bit in strin:
-        if not int(bit):
-            flipped += "1"
-        else:
-            flipped += "0"
-    return flipped
-
-
-print(convert32bit(1))
-print(flip("00000000000000000000000000000001"))
-print(int("11111111111111111111111111111110", 2))
-
-print(int(flip(convert32bit(1)), 2))
+        letter = strin[i]
+print(deletions)
