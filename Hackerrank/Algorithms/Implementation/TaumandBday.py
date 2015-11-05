d9489176d59788199c1c9469c171565b56e547c8
alohageck0 <royalfiish@gmail.com>
2015-11-04 22:57:28 -0800

    11/4/15

--- Hackerrank/Algorithms/Implementation/TaumandBday.py
+++ Hackerrank/Algorithms/Implementation/TaumandBday.py
@@ -3,7 +3,10 @@ for i in range(loops):
     blackwhite = [int(x) for x in input().split(" ")]
     costs = [int(x) for x in input().split(" ")]
     if abs(costs[0] - costs[1]) > costs[2]:
-    # write
+        if costs[0] > costs[1]:
+            amount = blackwhite[1] * costs[1] + blackwhite[0] * costs[1] + blackwhite[0] * costs[2]
+        else:
+            amount = blackwhite[0] * costs[0] + blackwhite[1] * costs[0] + blackwhite[1] * costs[2]
     else:
         amount = blackwhite[0] * costs[0] + blackwhite[1] + costs[1]
     print(amount)
