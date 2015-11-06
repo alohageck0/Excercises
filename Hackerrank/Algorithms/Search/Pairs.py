fad4328f143c7646aebacc6f950d4b2a1494cb39
alohageck0 <royalfiish@gmail.com>
2015-11-05 23:07:27 -0800

    11/5/15

---
+++ Hackerrank/Algorithms/Search/Pairs.py
@@ -0,0 +1,10 @@
+given = [int(x) for x in input().split(" ")]
+numbers = [int(x) for x in input().split(" ")]
+arr = sorted(numbers)
+count = 0
+for i in range(len(arr) - 1):
+    for j in range(i + 1, len(arr)):
+        if arr[j] - arr[i] == given[1]:
+            count += 1
+            break
+print(count)
