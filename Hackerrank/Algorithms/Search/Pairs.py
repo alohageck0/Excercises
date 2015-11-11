303de48c7ff3938bcbb0f7bffc1f0b17158b2253
alohageck0 <royalfiish@gmail.com>
2015-11-10 23:14:28 -0800

    11/10/15 correct indecies

--- Hackerrank/Algorithms/Search/Pairs.py
+++ Hackerrank/Algorithms/Search/Pairs.py
@@ -2,8 +2,21 @@ given = [int(x) for x in input().split(" ")]
 numbers = [int(x) for x in input().split(" ")]
 arr = sorted(numbers)
 point = int(len(arr) / 2)
-first = arr[:point]
-second = arr[point:]
-
+diff = given[1]
+down = arr[:point]
+up = arr[point:]
+count = 0
+i = len(down)
+j = 0
+while j < len(up):
+    tryDiff = abs(down[i] - up[j])
+    if tryDiff == diff and i > 0:
+        count += 1
+    else:
+        if tryDiff > diff:
+            i -= 1
+        else:
+            j += 1
+print(count)
 
 # 43253
