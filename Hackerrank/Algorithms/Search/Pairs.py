2f80ba86c5df4156cc87eb793cad7884fcdab67a
alohageck0 <royalfiish@gmail.com>
2015-11-10 22:57:41 -0800

    11/10/15

--- Hackerrank/Algorithms/Search/Pairs.py
+++ Hackerrank/Algorithms/Search/Pairs.py
@@ -1,13 +1,9 @@
 given = [int(x) for x in input().split(" ")]
 numbers = [int(x) for x in input().split(" ")]
 arr = sorted(numbers)
-count = 0
-for i in range(len(arr) - 1):
-    for j in range(i + 1, len(arr)):
-        if arr[j] - arr[i] == given[1]:
-            count += 1
-            break
-print(count)
+point = int(len(arr) / 2)
+first = arr[:point]
+second = arr[point:]
 
 
 # 43253
