ab20c845901a801fcda527de05aa696149c1f8ac
alohageck0 <royalfiish@gmail.com>
2015-11-11 21:58:08 -0800

    11/11/15

--- Hackerrank/Algorithms/Strings/Gemstones.py
+++ Hackerrank/Algorithms/Strings/Gemstones.py
@@ -1,11 +1,16 @@
 count = dict()
-
+number = 0
+arr = []
 loops = int(input())
 for i in range(loops):
-    strin = input()
-    for letter in strin:
+    arr.append(input())
+for strin in arr:
+    for letter in set(strin):
         if letter not in count.keys():
             count[letter] = 1
         else:
             count[letter] += 1
-print(count)
+for value in count.values():
+    if value >= loops:
+        number += 1
+print(number)
