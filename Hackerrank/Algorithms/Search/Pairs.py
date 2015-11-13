2bc6f15b964b4592af8779453a9f83598cf539a1
alohageck0 <royalfiish@gmail.com>
2015-11-12 23:23:12 -0800

    11/12/15

--- Hackerrank/Algorithms/Search/Pairs.py
+++ Hackerrank/Algorithms/Search/Pairs.py
@@ -1,22 +1,27 @@
 # given = [int(x) for x in input().split(" ")]
 # numbers = [int(x) for x in input().split(" ")]
 # arr = sorted(numbers)
+def pointer():
+    global count
+    down = arr[:point]
+    up = arr[point:]
+    i = len(down) - 1
+    j = 0
+    while j < len(up):
+        while abs(down[i] - up[j]) > diff and i > 0:
+            i -= 1
+        if abs(down[i] - up[j]) == diff:
+            count += 1
+        j += 1
+
+
 given = [5, 2]
 arr = [1, 2, 3, 4, 5]
-point = int(len(arr) / 2)
-diff = given[1]
-down = arr[:point]
-up = arr[point:]
-print(down, up)
 count = 0
-i = len(down) - 1
-j = 0
-while j < len(up):
-    while abs(down[i] - up[j]) > diff and i > 0:
-        i -= 1
-    if abs(down[i] - up[j]) == diff:
-        count += 1
-    j += 1
+diff = given[1]
+
+point = int(len(arr) / 2)
+
 print(count)
 # todo loop through elements in arrays
 # 43253
