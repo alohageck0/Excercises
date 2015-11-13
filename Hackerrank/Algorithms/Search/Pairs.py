c54d16dd5a6215837584e114d0a97f7e8f7724e8
alohageck0 <royalfiish@gmail.com>
2015-11-12 23:28:07 -0800

    11/12/15

--- Hackerrank/Algorithms/Search/Pairs.py
+++ Hackerrank/Algorithms/Search/Pairs.py
@@ -1,6 +1,8 @@
-# given = [int(x) for x in input().split(" ")]
-# numbers = [int(x) for x in input().split(" ")]
-# arr = sorted(numbers)
+given = [int(x) for x in input().split(" ")]
+numbers = [int(x) for x in input().split(" ")]
+arr = sorted(numbers)
+
+
 def pointer(point):
     global count
     down = arr[:point]
@@ -15,12 +17,8 @@ def pointer(point):
         j += 1
 
 
-given = [5, 2]
-arr = [1, 2, 3, 4, 5]
 count = 0
 diff = given[1]
-
-point = int(len(arr) / 2)
 for i in range(1, len(arr) - 1):
     pointer(i)
 print(count)
--- sandbox.py
+++ sandbox.py
@@ -1,8 +1,24 @@
-arr = [1, 2, 3, 4, 5, 6, 7]
-point = int(len(arr) / 2) + 1
-down = arr[:point]
-up = arr[point:]
-i = len(down) - 1
-print(down, up)
-print(len(down), len(up))
-print(i)
+# given = [int(x) for x in input().split(" ")]
+# numbers = [int(x) for x in input().split(" ")]
+# arr = sorted(numbers)
+def pointer(point):
+    global count
+    down = arr[:point]
+    up = arr[point:]
+    i = len(down) - 1
+    j = 0
+    while j < len(up):
+        while abs(down[i] - up[j]) > diff and i > 0:
+            i -= 1
+        if abs(down[i] - up[j]) == diff:
+            count += 1
+        j += 1
+
+
+given = [5, 2]
+arr = [1, 2, 3, 4, 5]
+count = 0
+diff = given[1]
+for i in range(1, len(arr) - 1):
+    pointer(i)
+print(count)
