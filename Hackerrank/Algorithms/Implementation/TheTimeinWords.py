61348cb9967a62a60830a86bdbdeec071f58c40a
alohageck0 <royalfiish@gmail.com>
2015-11-21 01:02:08 -0800

    11/21/15 improve

--- Hackerrank/Algorithms/Implementation/TheTimeinWords.py
+++ Hackerrank/Algorithms/Implementation/TheTimeinWords.py
@@ -1,13 +1,11 @@
-# hour = int(input())
-# minutes = int(input())
-hour = 7
-minutes = 45
+hour = int(input())
+minutes = int(input())
 words = {1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven",
          8: "eight", 9: "nine", 10: "ten", 11: "eleven", 12: "twelve",
          13: "thirteen", 14: "fourteen", 15: "quarter", 16: "sixteen", 18: "eighteen", 19: "nineteen",
          20: "twenty", 30: "half"}
 if minutes == 00:
-    print(words[hour] + " o'clock")
+    print(words[hour] + " o' clock")
 
 
 elif minutes < 30:
@@ -26,7 +24,7 @@ elif minutes > 30:
     elif minutes == 1:
         print(words[minutes] + " minute to " + words[hour + 1])
     elif minutes == 15:
-        print(words[minutes] + " to " + words[hour])
+        print(words[minutes] + " to " + words[hour + 1])
     else:
         print(words[minutes] + " minutes to " + words[hour + 1])
 else:
--- sandbox.py
+++ sandbox.py
@@ -1,11 +1,33 @@
-# given = [int(x) for x in input().split(" ")]
-# numbers = [int(x) for x in input().split(" ")]
-#
-given = [5, 2]
-numbers = [3, 4, 2, 1, 5]
-result = dict()
-for i in numbers:
-    if i not in result.keys():
-        result[i] = 1
-result1 = [1 for i in result.keys() if i + given[1] in result.keys()]
-print(len(result1))
+# hour = int(input())
+# minutes = int(input())
+hour = 5
+minutes = 45
+words = {1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven",
+         8: "eight", 9: "nine", 10: "ten", 11: "eleven", 12: "twelve",
+         13: "thirteen", 14: "fourteen", 15: "quarter", 16: "sixteen", 18: "eighteen", 19: "nineteen",
+         20: "twenty", 30: "half"}
+if minutes == 00:
+    print(words[hour] + " o'clock")
+
+
+elif minutes < 30:
+    if str(minutes)[0] == "2":
+        print(words[20] + " " + words[int(str(minutes)[1])] + " minutes past " + words[hour])
+    elif minutes == 1:
+        print(words[minutes] + " minute past " + words[hour])
+    elif minutes == 15:
+        print(words[minutes] + " past " + words[hour])
+    else:
+        print(words[minutes] + " minutes past " + words[hour])
+elif minutes > 30:
+    minutes = 60 - minutes
+    if str(minutes)[0] == "2":
+        print(words[20] + " " + words[int(str(minutes)[1])] + " minutes to " + words[hour + 1])
+    elif minutes == 1:
+        print(words[minutes] + " minute to " + words[hour + 1])
+    elif minutes == 15:
+        print(words[minutes] + " to " + words[hour + 1])
+    else:
+        print(words[minutes] + " minutes to " + words[hour + 1])
+else:
+    print(words[minutes] + " past " + words[hour])
