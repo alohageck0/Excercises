3ab75a7e1f9661c9ed58f6d4f8c5dea28f6e13d3
alohageck0 <royalfiish@gmail.com>
2015-11-21 00:56:33 -0800

    11/21/15

--- Hackerrank/Algorithms/Implementation/TheTimeinWords.py
+++ Hackerrank/Algorithms/Implementation/TheTimeinWords.py
@@ -1,10 +1,27 @@
 # hour = int(input())
-# minutes = [int(x) for x in input().split(" ")]
-hour = 12
-minutes = 00
+# minutes = int(input())
+hour = 7
+minutes = 30
 words = {1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven",
          8: "eight", 9: "nine", 10: "ten", 11: "eleven", 12: "twelve",
          13: "thirteen", 14: "fourteen", 15: "quarter", 16: "sixteen", 18: "eighteen", 19: "nineteen",
          20: "twenty", 30: "half"}
 if minutes == 00:
     print(words[hour] + " o'clock")
+elif minutes < 30:
+    if str(minutes)[0] == "2":
+        print(words[20] + " " + words[int(str(minutes)[1])] + " minutes past " + words[hour])
+    elif minutes == 1:
+        print(words[minutes] + " minute past " + words[hour])
+    else:
+        print(words[minutes] + " minutes past " + words[hour])
+elif minutes > 30:
+    minutes = 60 - minutes
+    if str(minutes)[0] == "2":
+        print(words[20] + " " + words[int(str(minutes)[1])] + " minutes to " + words[hour + 1])
+    elif minutes == 1:
+        print(words[minutes] + " minute to " + words[hour + 1])
+    else:
+        print(words[minutes] + " minutes to " + words[hour + 1])
+else:
+    print(words[minutes] + " past " + words[hour])
