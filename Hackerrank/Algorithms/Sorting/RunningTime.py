3d60e5e5c519c8c1eee871fa6db2fb3a4dacb92c
alohageck0 <royalfiish@gmail.com>
2015-11-26 13:38:05 -0800

    11/26/15

---
+++ Hackerrank/Algorithms/Sorting/RunningTime.py
@@ -0,0 +1,28 @@
+arr = [2, 1, 3, 1, 2]
+len = len(arr)
+# len = int(input())
+# arr = [int(x) for x in input().split(' ')]
+count = 0
+for i in range(1, len):
+    element = arr[i]
+    if element < arr[i - 1]:
+        arr[i] = arr[i - 1]
+        for j in reversed(range(i)):
+            if arr[j - 1] < element:
+                arr[j] = element
+                count += 1
+                break
+            else:
+                if j == 0:
+                    arr[j] = element
+                    count += 1
+                else:
+                    arr[j] = arr[j - 1]
+                    count += 1
+                continue
+                # print(' '.join([str(x) for x in arr]))
+
+    else:
+        pass
+        # print(' '.join([str(x) for x in arr]))
+print(count)
