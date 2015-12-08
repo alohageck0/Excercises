23b5c17ae348fbd1b9a3cece113729d4e1c1c4c2
alohageck0 <royalfiish@gmail.com>
2015-12-07 20:23:46 -0800

    12/07/15

--- sandbox.py
+++
@@ -1,7 +0,0 @@
-# given = int(input())
-# numbers = [int(x) for x in input().split(" ")]
-# numbers = [1, 2, 3]
-test = []
-for i in range(len(numbers)):
-    test.append(str(numbers[len(numbers) - i - 1]))
-print(" ".join(test))
