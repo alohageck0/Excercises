78b6169c07f77eb440ea06cda1fc4c127c83992f
alohageck0 <royalfiish@gmail.com>
2016-01-01 12:46:46 -0800

    01/1/15

---
+++ Hackerrank/30Day/Day1.py
@@ -0,0 +1,28 @@
+# Day 1: Data Types!
+
+# 5.35
+print("Primitive : float")
+
+# 'a'
+print("Primitive : char")
+
+# false
+print("Primitive : boolean")
+
+# 100
+print("Primitive : int")
+
+# "I am a code monkey"
+print("Referenced : string")
+
+# true
+print("Primitive : boolean")
+
+# 17.3
+print("Primitive : float")
+
+# 'c'
+print("Primitive : char")
+
+# "derp"
+print("Referenced : string")
